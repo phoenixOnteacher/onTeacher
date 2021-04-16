e6dcc6f8a1e6c3ab738c24970b3989344b83ec57
kmathl96 <kmathl96@naver.com>
2021-04-16 15:59:28 +0900

    chore : Upload SQL files 1. createTable2.sql : 이전의 테이블삭제 및 수정된 테이블 생성 2. insertCategoryData.sql : 카테고리 데이터 생성 3. dropTable.sql : 테이블 삭제

---
+++ docs/insertCategoryData.sql
@@ -0,0 +1,26 @@
+-- Insert data into Category
+insert into HighCategory values (HighCategoryIdSeq.nextval, '주요과목');
+insert into HighCategory values (HighCategoryIdSeq.nextval, '예체능');
+insert into HighCategory values (HighCategoryIdSeq.nextval, '상담');
+insert into HighCategory values (HighCategoryIdSeq.nextval, '어학');
+insert into HighCategory values (HighCategoryIdSeq.nextval, '대회 및 자격증');
+
+insert into LowCategory values (LowCategoryIdSeq.nextval, 1, '국어');
+insert into LowCategory values (LowCategoryIdSeq.nextval, 1, '영어');
+insert into LowCategory values (LowCategoryIdSeq.nextval, 1, '수학');
+insert into LowCategory values (LowCategoryIdSeq.nextval, 1, '사회');
+insert into LowCategory values (LowCategoryIdSeq.nextval, 1, '과학');
+insert into LowCategory values (LowCategoryIdSeq.nextval, 2, '음악');
+insert into LowCategory values (LowCategoryIdSeq.nextval, 2, '체육');
+insert into LowCategory values (LowCategoryIdSeq.nextval, 2, '미술');
+insert into LowCategory values (LowCategoryIdSeq.nextval, 2, '무용');
+insert into LowCategory values (LowCategoryIdSeq.nextval, 3, '진로 상담');
+insert into LowCategory values (LowCategoryIdSeq.nextval, 3, '입시 상담');
+insert into LowCategory values (LowCategoryIdSeq.nextval, 4, 'TEPS/TOEFL/TOEIC');
+insert into LowCategory values (LowCategoryIdSeq.nextval, 4, 'JPT/JLPT');
+insert into LowCategory values (LowCategoryIdSeq.nextval, 4, 'HSK');
+insert into LowCategory values (LowCategoryIdSeq.nextval, 4, '기타제2외국어');
+insert into LowCategory values (LowCategoryIdSeq.nextval, 5, '올림피아드');
+insert into LowCategory values (LowCategoryIdSeq.nextval, 5, 'IT/컴퓨터');
+insert into LowCategory values (LowCategoryIdSeq.nextval, 5, '한국사능력검정시험');
+insert into LowCategory values (LowCategoryIdSeq.nextval, 5, '기타 대회 및 자격증');
