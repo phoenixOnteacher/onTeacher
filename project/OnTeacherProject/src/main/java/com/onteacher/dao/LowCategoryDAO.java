e36f03d0a8bd2357a4bf35dcf27960cf8f93f193
minami-cs <mina9301@gmail.com>
2021-04-20 16:22:13 +0900

    chore : adapt course register feature to the newly amended server 1. highcategory와 lowcategory mapper 분리 2. highcategorydao와 lowcategorydao 분리 3. coursemanageservice와 coursemanageserviceimpl 수정

--- project/OnTeacherProject/src/main/java/com/onteacher/dao/LowCategoryDAO.java
+++ project/OnTeacherProject/src/main/java/com/onteacher/dao/LowCategoryDAO.java
@@ -1,10 +1,14 @@
 package com.onteacher.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
+
+import com.onteacher.vo.LowCategory;
 
 @Mapper
 @Repository("lowCategoryDAO")
 public interface LowCategoryDAO {
-
+	public List<LowCategory> selectLowCategory(int high_category_id) throws Exception;
 }
--- project/OnTeacherProject/src/main/java/com/onteacher/service/CourseManageServiceImpl.java
+++ project/OnTeacherProject/src/main/java/com/onteacher/service/CourseManageServiceImpl.java
@@ -3,12 +3,12 @@ package com.onteacher.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.onteacher.dao.CourseManageDAO;
+import com.onteacher.dao.HighCategoryDAO;
+import com.onteacher.dao.LowCategoryDAO;
 import com.onteacher.vo.HighCategory;
 import com.onteacher.vo.LowCategory;
 
@@ -17,15 +17,18 @@ import com.onteacher.vo.LowCategory;
 public class CourseManageServiceImpl implements CourseManageService {
 
 	@Autowired
-	CourseManageDAO courseManageDAO;
+	private HighCategoryDAO highCategoryDAO;
+	
+	@Autowired
+	private LowCategoryDAO lowCategoryDAO;
 
 	@Override
 	public List<HighCategory> getHighCategory() throws Exception {
-		return courseManageDAO.selectHighCategory();
+		return highCategoryDAO.selectHighCategory();
 	}
 
 	@Override
 	public List<LowCategory> getLowCategory(int high_category_id) throws Exception {
-		return courseManageDAO.selectLowCategory(high_category_id);
+		return lowCategoryDAO.selectLowCategory(high_category_id);
 	}
 }
