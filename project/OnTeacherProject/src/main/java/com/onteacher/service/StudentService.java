951ee9abc885d98de9bb8aac389a846aee1ef727
seungbin <gtb2012@naver.com>
2021-04-20 09:57:05 +0900

    create sql

--- project/OnTeacherProject/src/main/java/com/onteacher/service/StudentService.java
+++ project/OnTeacherProject/src/main/java/com/onteacher/service/StudentService.java
@@ -1,5 +1,8 @@
 package com.onteacher.service;
 
+import com.onteacher.vo.Student;
+
 public interface StudentService {
+	public void makeId(Student std) throws Exception;
 	
 }
--- project/OnTeacherProject/src/main/java/com/onteacher/service/StudentServiceImpl.java
+++ project/OnTeacherProject/src/main/java/com/onteacher/service/StudentServiceImpl.java
@@ -6,11 +6,24 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.onteacher.dao.StudentDAO;
+import com.onteacher.vo.Student;
 
+/**
+ * @author kasus
+ *
+ */
 @Service
 @Transactional(propagation = Propagation.REQUIRED)
 public class StudentServiceImpl implements StudentService {
-
 	@Autowired
 	StudentDAO studentDAO;
+
+	@Override
+	public void makeId(Student std) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	
 }
