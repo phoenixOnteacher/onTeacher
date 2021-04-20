4dd23519543bd20d167b72863839a0ad7df08a0f
minami-cs <mina9301@gmail.com>
2021-04-20 20:45:19 +0900

    chore : update course.xml and course_register.js 1. course mapper sql insert문 작성 2. ajax 수정

--- project/OnTeacherProject/src/main/webapp/resources/js/course_register.js
+++ project/OnTeacherProject/src/main/webapp/resources/js/course_register.js
@@ -1,17 +1,16 @@
 $(function() {
-	var select = "<option>선택해주세요</option>";
+	var select = '<option value="">선택해주세요</option>';
 	
 	$("#highcategory").change(function() {
-		$("#lowcategory").children().remove();
+		$("#lowcategory").empty();
 		if($("#hightcategory").val() == "") { //select의 value가 ""이면 "선택해주세요"만 보여주도록
-			$("#lowcategory").find("option").remove().end.append(select);
+			$("#lowcategory").end.append(select);
 		} else {
 			comboChange($(this).val());
 		}
 	});
 	
 	function comboChange(highcategoryid) {
-		
 		$.ajax({
 			type: "GET",
 			url: "http://localhost:8090/highcategory",
