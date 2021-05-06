53429d6e5010ee58343abcfe4d9721d612bff221
kmathl96 <kmathl96@naver.com>
2021-05-06 22:35:14 +0900

    docs : Update alterTable.sql 1. Reply 테이블 Sequence 추가

--- docs/alterTable.sql
+++ docs/alterTable.sql
@@ -28,3 +28,6 @@ alter table notification drop column from_id;
 alter table notification add is_checked char(1) default 0 not null;
 -- Homework : filename 길이 변경
 alter table homework modify filename varchar2(1000);
+
+-- 5/6 Reply : Sequence 추가
+create sequence ReplyIdSeq start with 1;
