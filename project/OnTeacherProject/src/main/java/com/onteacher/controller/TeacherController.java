5a741d88c396b6e902ed4ac5e12d1834b2450e18
kmathl96 <kmathl96@naver.com>
2021-05-11 20:46:38 +0900

    style : import 수정

--- project/OnTeacherProject/src/main/java/com/onteacher/controller/TeacherController.java
+++ project/OnTeacherProject/src/main/java/com/onteacher/controller/TeacherController.java
@@ -18,7 +18,6 @@ import javax.servlet.http.HttpSession;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
